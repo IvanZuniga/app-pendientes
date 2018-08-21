c0e1efb11f12137cf3b42e01379f60a68a9cbb6a
Lydex <ivanzuniga.gu@gmail.com>
2018-08-21 12:59:03 -0600

    cambios

--- src/providers/pendientes/pendientes.ts
+++ src/providers/pendientes/pendientes.ts
@@ -3,32 +3,34 @@ import { Injectable } from '@angular/core';
 import { APP_URL } from '../../config/url.servicios';
 import { ToastController } from 'ionic-angular';
 import { PendienteData } from '../../models/pendiente-data.model';
-
+import { LoadingController } from 'ionic-angular';
 
 @Injectable()
 export class PendientesProvider {
 
   lista_pendientes:any = [];
-  
 
-  constructor(public http: HttpClient, public toastCtrl: ToastController) {}
+
+  constructor(public http: HttpClient, public toastCtrl: ToastController, public loadingCtrl: LoadingController) {}
 
   cargarPendientes(){
     let url = APP_URL + "/api/pendientes";
 
     return new Promise( resolve => {
+
       this.http.get( url ).subscribe( data => {
         this.lista_pendientes = data;
         resolve();
       }, err => {
         this.toastCtrl.create({
-          message: 'Error: no se han podido obtener los datos.'
+          message: 'Error: no se han podido obtener los datos.',
+          duration: 3000
         }).present();
         console.log(err);
       });
 
     });
-    
+
   }
 
   agregar( titulo:string ){
@@ -36,12 +38,22 @@ export class PendientesProvider {
 
 
     return new Promise( resolve => {
+      const loading = this.loadingCtrl.create({
+        content: 'Insertando...',
+        dismissOnPageChange: false
+      });
+
+      loading.present();
+
       this.http.post( url , {titulo} ).subscribe( (data) => {
         this.cargarPendientes();
+        loading.dismiss();
         resolve();
       }, err => {
+        loading.dismiss();
         this.toastCtrl.create({
-          message: 'Error: no se ha podido agregar el pendiente.'
+          message: 'Error: no se ha podido agregar el pendiente.',
+          duration: 3000
         }).present();
         console.log(err);
       });
@@ -54,12 +66,23 @@ export class PendientesProvider {
     let url = APP_URL + "/api/pendientes/"+pendiente.id;
 
     return new Promise( resolve => {
+
+      const loading = this.loadingCtrl.create({
+        content: 'Editando...',
+        dismissOnPageChange: false
+      });
+
+      loading.present();
+
       this.http.put( url , pendiente ).subscribe( () => {
         this.cargarPendientes();
+        loading.dismiss();
         resolve();
       }, err => {
+        loading.dismiss();
         this.toastCtrl.create({
-          message: 'Error: no se ha podido editar el pendiente.'
+          message: 'Error: no se ha podido editar el pendiente.',
+          duration: 3000
         }).present();
         console.log(err);
       });
@@ -76,7 +99,8 @@ export class PendientesProvider {
         resolve();
       }, err => {
         this.toastCtrl.create({
-          message: 'Error: no se ha podido borrar el pendiente.'
+          message: 'Error: no se ha podido borrar el pendiente.',
+          duration: 3000
         }).present();
         console.log(err);
       });
